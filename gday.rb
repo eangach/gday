33a3ab58210c7ba88109005985099e1f5b303521
Thomas Behrndt <thomas.behrndt@me.com>
2013-10-11 14:05:03 +1100

    Using a module as a namespace

--- gday.rb
+++ gday.rb
@@ -1,23 +1,21 @@
 #!/usr/bin/env ruby
 #
-# Using a module as a mixin
+# Using a module as a namespace
 module Gday
-  def bye
-    puts "Cheerio"
-  end
-end
+  class Greeting
+    def bye
+      puts "Cheerio"
+    end
 
-class Greeting
-  include Gday
-
-  def hi
-    puts "G'day!"
+    def hi
+      puts "G'day!"
+    end
   end
 end
 
 # Run if the file is executed directly
 if __FILE__ == $0
-  greeting = Greeting.new
+  greeting = Gday::Greeting.new
   greeting.hi
   greeting.bye
 end
