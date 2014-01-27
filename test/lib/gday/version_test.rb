50983b4cad77ca9d96c42adfb8e119b5fac81995
Thomas Behrndt <thomas.behrndt@me.com>
2014-01-27 16:34:48 +1100

    Add extra tests and assertions for VERSION

--- test/lib/gday/version_test.rb
+++ test/lib/gday/version_test.rb
@@ -6,4 +6,9 @@ describe Gday, 'VERSION' do
     Gday::VERSION.wont_be_nil
   end
 
+  it 'has 3 or 4 dot separated parts' do
+    Gday::VERSION.split('.').size.must_be :>=, 3
+    Gday::VERSION.split('.').size.must_be :<=, 4
+  end
+
 end
